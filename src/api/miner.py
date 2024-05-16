f284533f957c23e72d5d21f677fc77f2316157ef
Sri Bala <srirocks2020@sri-mac.local>
2024-05-15 18:49:10 -0700

    miner logic adjusted

--- src/api/miner.py
+++ src/api/miner.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from src.api import auth
 from src import database as db
 import sqlalchemy
-import rand
+import random
 
 router = APIRouter(
     prefix="/civilian/miner",
@@ -34,9 +34,9 @@ def post_substance(citizen_id: int, substance: Substance):
                                            WHERE type = 'substances' AND citizen_id = :id"""),
                                     {'id' : citizen_id})
         
-        connection.execute(sqlalchemy.text("""UPDATE substances SET quantity = quantity - :quant
+        connection.execute(sqlalchemy.text("""UPDATE substances SET quantity = quantity - :mined
                                            WHERE name = :name"""),
-                                    {'quant' : substance.quantity, 'name' : substance.name})
+                                    {'mined' : substance.quantity, 'name' : substance.name})
     print(f"substances delievered: {substance}") # each miner mines a single planets substance, only one returned
     return "OK"
 
@@ -44,31 +44,26 @@ def post_substance(citizen_id: int, substance: Substance):
 @router.post("/plan/{citizen_id}")
 def create_miner_plan(citizen_id: int):
     with db.engine.begin() as connection:
-        substances = connection.execute(sqlalchemy.text('''SELECT substances.name, substances.price, substances.planet, substances.quantity FROM substances 
-                                                        LEFT JOIN citizens ON substances.planet = citizens.planet WHERE citizens.id = id''')
-                                                        , [{"id": citizen_id}]).scalar()
+        subst_data = connection.execute(sqlalchemy.text('''SELECT substances.name, substances.planet, substances.quantity, substances.price from substances JOIN citizens ON substances.planet = citizens.planet WHERE citizens.id = :id''')
+                                                        , [{"id": citizen_id}]).first()
         
-        # voidex = connection.execute(sqlalchemy.text('''SELECT quantity FROM inventory 
-        #                                                 WHERE type = 'voidex' AND citizens.id = id''')
-        #                                                 , [{"id": citizen_id}]).scalar()
-    
-        cap_mining = substances[3] * 0.25 # can at most mine 1/4 of the planet
-        if cap_mining == 0: return {}
-        mining_amt = rand.randint(0, cap_mining)
+        cap_mining = round(subst_data[3] * 0.25) # can at most mine 1/4 of the planet
+        if cap_mining == 0: return {} # planet substance depleted, maybe we add substance after certain time
+        mining_amt = random.randint(0, cap_mining)
 
 
         # substance added to miner inventory
         connection.execute(sqlalchemy.text("""INSERT INTO inventory (citizen_id, type, quantity, name, status) 
                                             VALUES (:id, 'substances', :quant, :name, 'owned')"""),
-                                   {'id' : citizen_id, 'quant' : mining_amt, 'name' : substances[0]})
+                                   {'id' : citizen_id, 'quant' : mining_amt, 'name' : subst_data[0]})
         
     return [
         #Substance(substances[0], substances[2], mining_amt, substances[1])
         {
-            "name": substances[0],
-            "planet_id": substances[2],
+            "name": subst_data[0],
+            "planet_id": subst_data[1],
             "quantity": mining_amt,
-            "price": substances[1]
+            "price": subst_data[3]
         }
     ]
 
