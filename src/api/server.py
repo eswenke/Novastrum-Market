7c32fd289756473330d6d71bdbeb2490f25db67d
Sri Bala <srirocks2020@sri-mac.local>
2024-06-05 10:46:03 -0700

    govt consolidating, debugging here and there

--- src/api/server.py
+++ src/api/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, exceptions
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
-from src.api import admin, chemist, citizen, govt, info, inventory, market, transaction, miner, bids, narco
+from src.api import admin, chemist, citizen, govt, inventory, market, transaction, miner, bids, narco
 import json
 import logging
 import sys
@@ -37,8 +37,7 @@ app.include_router(transaction.router)
 app.include_router(market.router)
 app.include_router(govt.router)
 app.include_router(chemist.router)
-# app.include_router(admin.router)
-# app.include_router(info.router)
+app.include_router(admin.router)
 app.include_router(narco.router)
 app.include_router(miner.router)
 app.include_router(citizen.router)
