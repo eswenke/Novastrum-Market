9cc0a13b3707934927794202f5c03118aa673002
Bryce Kennedy <76964106+bkennedy06@users.noreply.github.com>
2024-06-05 00:42:44 -0700

    missing a .scalar()

--- src/api/narco.py
+++ src/api/narco.py
@@ -27,7 +27,7 @@ def post_drugs_done(narcos_delivered: list[Narcotic], citizen_id:int):
                                                           WHERE citizen_id = :cit_id
                                                           AND name = :narco_name
                                                           AND quantity > 0);"""),
-                                    {'narco_name' : narco.name, 'cit_id' : citizen_id})
+                                    {'narco_name' : narco.name, 'cit_id' : citizen_id}).scalar()
             # Determining coolness, quantity * rarity
             coolness = narco.quantity * connection.execute(sqlalchemy.text("""SELECT COALESCE((
                                                                      SELECT rarity FROM narcos 
